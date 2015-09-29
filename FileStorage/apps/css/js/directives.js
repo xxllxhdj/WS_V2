509214636571dc50f3f603fa1446a952ef998a51
xuxl <xuxle@yonyou.com>
2015-09-29 16:34:37 +0800

    css.weather:渐变

--- FileStorage/apps/css/js/directives.js
+++ FileStorage/apps/css/js/directives.js
@@ -17,16 +17,32 @@ define(['app'], function (app) {
             compile: function(element, attr) {
                 var rangeContainer = angular.element('<div class="range-container"></div>'),
                     rag;
+                var gradientColor = gradientColor([255, 165, 0], [255, 0, 0], 85);
                 for (var i = 8; i <= 92; i++) {
                     rag = angular.element('<div class="rag"></div>');
                     rag[0].style['-webkit-transform'] = 'rotate(' + 3.6 * i + 'deg)';
                     rag[0].style['transform'] = 'rotate(' + 3.6 * i + 'deg)';
+                    rag[0].style['background'] = gradientColor[i - 8];
                     rangeContainer.append(rag);
                 }
                 element.append(rangeContainer);
 
                 return { pre: prelink };
                 function prelink($scope, $element, $attr) {}
+
+                function gradientColor(startColor, endColor, step) {
+                    var sR = (endColor[0] - startColor[0]) / step,
+                        sG = (endColor[1] - startColor[1]) / step,
+                        sB = (endColor[2] - startColor[2]) / step;
+
+                    var colorArr = [];
+                    for(var i = 0; i < step; i++) {
+                        var rgb = 'rgb(' + parseInt(sR * i + startColor[0]) + ',' +
+                            parseInt(sG * i + startColor[1]) + ',' + parseInt(sB * i + startColor[2]) + ')';
+                        colorArr.push(rgb);
+                    }
+                    return colorArr;
+                }
             }
         };
     }]);
